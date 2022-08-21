0560245642e1359d4207b1d538b7be6006766425
Santos Gallegos <stsewd@proton.me>
2022-08-21 17:12:59 -0500

    Improve solution

--- src/bin/monk_and_sort_alg.rs
+++ src/bin/monk_and_sort_alg.rs
@@ -1,34 +1,42 @@
 // https://www.hackerearth.com/problem/algorithm/monk-and-sorting-algorithm-3aa7826d/
 
-use std::cmp;
 use std::io::{self, BufRead, Write};
 
-fn getkey(item: &str, i: usize) -> usize {
-    let to = i * 5;
-    let from = to - 4;
-    if item.len() < from {
-        return 0;
-    }
-    let end = item.len() - from;
-    let start = item.len() - cmp::min(item.len(), to);
-    item[start..end + 1].parse().unwrap()
+fn getkey(n: usize, i: usize) -> usize {
+    let base: usize = 10;
+    let n = n / base.pow(((i - 1) * 5) as u32);
+    n % 100000
 }
 
-fn solve(array: &[String], output: &mut impl Write) {
-    let mut i = 1;
+fn solve(array: &[usize], output: &mut impl Write) {
     let mut array = array.to_vec();
-    loop {
-        let mut sum = 0;
-        array.sort_by_key(|x| {
-            let r = getkey(x, i);
-            sum += r;
-            r
-        });
-        if sum == 0 {
-            break;
+    let mut result = vec![0; array.len()];
+    let max = array.iter().max().unwrap().to_string().len();
+    let (a, b) = (max / 5, max % 5);
+    let n = a + (if b != 0 { 1 } else { 0 });
+    for i in 1..=n {
+        let mut counter: [usize; 100000] = [0; 100000];
+        // Assign.
+        for &element in &array {
+            let k = getkey(element, i);
+            counter[k] += 1;
         }
-        writeln!(output, "{}", array.join(" ")).unwrap();
-        i += 1;
+        // Calculate positions.
+        for j in 1..counter.len() {
+            counter[j] += counter[j - 1];
+        }
+        // Put elements in position respecting the current order.
+        for j in (0..array.len()).rev() {
+            let k = getkey(array[j], i);
+            result[counter[k] - 1] = array[j];
+            counter[k] -= 1;
+        }
+        // Print answer.
+        for element in &result {
+            write!(output, "{} ", element).unwrap();
+        }
+        writeln!(output).unwrap();
+        array = result.clone();
     }
 }
 
@@ -36,7 +44,10 @@ pub fn main() {
     let stdin = io::stdin();
     let mut iter = stdin.lock().lines().map(|x| x.unwrap());
     let line = iter.nth(1).unwrap();
-    let array: Vec<String> = line.split_whitespace().map(|x| x.to_owned()).collect();
+    let array: Vec<usize> = line
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
     solve(&array, &mut io::stdout());
 }
 
@@ -47,14 +58,11 @@ mod tests {
     #[test]
     fn test_solve() {
         let mut output = Vec::new();
-        let array: Vec<String> = ["213456789", "167890", "123456789"]
-            .iter()
-            .map(|&x| x.to_owned())
-            .collect();
+        let array: Vec<usize> = vec![213456789, 167890, 123456789];
         solve(&array, &mut output);
         assert_eq!(
             &output,
-            b"213456789 123456789 167890\n167890 123456789 213456789\n"
+            b"213456789 123456789 167890 \n167890 123456789 213456789 \n"
         );
     }
 }
